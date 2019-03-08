0011586ed80db56e9547192d9c2e66b37d7280e2
Jamal Fazlur Rahman <jamalfazlurrahman@JAMAL-DOKUs-iMac.local>
2019-03-08 18:07:29 +0700

    Create: converter JSON to Java, dan sebaliknya

--- src/main/java/com/percobaan_satu/Main.java
+++ src/main/java/com/percobaan_satu/Main.java
@@ -1,6 +1,7 @@
 package com.percobaan_satu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +23,19 @@ public class Main {
 
         // Tampilkan Object dalam bentuk JSON
         System.out.println("Setalah ubah ke JSON: " + JsonConverter.toJson(Jamal)); // {"nama":"Jamal Fazlur","umur":25,"berat":70}
+
+
+        // Buat Object String JSON
+        String Manusia =    "{ \"nama\": \"Fazlur\"," +
+                            " \"umur\": 23  }";
+
+        System.out.println("String Java" + Manusia);
+
+        // Convert dari JSON ke Object Java
+        System.out.println(JsonConverter.fromJson(Manusia, ObjeckOrang.class));
+
+        //String ManusiaBaru = JsonConverter.fromJson(Manusia, ObjeckOrang.class).getNama();
+        System.out.println(JsonConverter.fromJson(Manusia, ObjeckOrang.class).getNama());
 
     }
 }
